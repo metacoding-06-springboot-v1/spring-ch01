597ead449e66091fff447403bb15901dec4882bf
metacoding <getinthere@naver.com>
2026-01-28 16:56:07 +0900

    toList 변경

--- src/main/java/com/metacoding/springv1/board/BoardService.java
+++ src/main/java/com/metacoding/springv1/board/BoardService.java
@@ -15,11 +15,11 @@ public class BoardService {
     public List<BoardResponse.DTO> 게시글목록() {
 
         List<Board> boards = boardRepository.findAll(); // board 엔티티 조회
-        List<BoardResponse.DTO> dtoList = boards.stream()  
-	            .map(board -> new BoardResponse.DTO(board))
-	            .collect(Collectors.toList());
+        List<BoardResponse.DTO> dtoList = boards.stream()
+                .map(board -> new BoardResponse.DTO(board))
+                .toList();
         return dtoList;
-    }    
+    }
 
     public BoardResponse.DetailDTO 게시글상세(Integer id) {
         Board board = boardRepository.findById(id).get(); // pk 로 조회
@@ -28,25 +28,25 @@ public class BoardService {
     }
 
     @Transactional
-    public void 게시글추가(BoardRequest.SaveDTO requestDTO){
-        Board board = requestDTO.toEntity(); //DTO -> 엔티티
+    public void 게시글추가(BoardRequest.SaveDTO requestDTO) {
+        Board board = requestDTO.toEntity(); // DTO -> 엔티티
         boardRepository.save(board);
     }
-    
+
     @Transactional
-    public void 게시글삭제(Integer id){
+    public void 게시글삭제(Integer id) {
         Board board = boardRepository.findById(id).get();
         boardRepository.delete(board);
     }
 
-    public BoardResponse.DTO 게시글수정폼(Integer id){
+    public BoardResponse.DTO 게시글수정폼(Integer id) {
         Board board = boardRepository.findById(id).get();
         BoardResponse.DTO dto = new BoardResponse.DTO(board);
         return dto;
     }
 
     @Transactional
-    public void 게시글수정(Integer id, BoardRequest.UpdateDTO requestDTO){
+    public void 게시글수정(Integer id, BoardRequest.UpdateDTO requestDTO) {
         Board board = boardRepository.findById(id).get();
         // 더티 체킹
         board.setTitle(requestDTO.getTitle());
