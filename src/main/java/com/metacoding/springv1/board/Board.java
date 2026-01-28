1cd3ed6388764c66fd7dc458261304a10cb8d075
metacoding <getinthere@naver.com>
2026-01-28 17:40:45 +0900

    빌더수정

--- src/main/java/com/metacoding/springv1/board/Board.java
+++ src/main/java/com/metacoding/springv1/board/Board.java
@@ -9,21 +9,24 @@ import lombok.*;
 
 @NoArgsConstructor // 기본 생성자
 @Data
-@Entity  
-@Table(name="board_tb") 
+@Entity
+@Table(name = "board_tb")
 public class Board {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)  // 자동 증가
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가
     private Integer id;
-    private  String title;
-    private  String content;
+    private String title;
+    private String content;
 
     @CreationTimestamp // 자동으로 현재 시간 저장
-    private  Timestamp createdAt;
+    private Timestamp createdAt;
 
     @Builder // 객체 생성 용도
-    public Board(String title, String content) {
+    public Board(Integer id, String title, String content, Timestamp createdAt) {
+        this.id = id;
         this.title = title;
         this.content = content;
+        this.createdAt = createdAt;
     }
+
 }
